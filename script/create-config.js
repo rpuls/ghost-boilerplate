addee80877da9ecffa43d93a20d43ce2cbee2bb3
Webfam <petersonnjiiri01@gmail.com>
2025-05-07 21:57:05 +0300

    Update create-config.js

--- script/create-config.js
+++ script/create-config.js
@@ -36,52 +36,26 @@ function createConfig() {
   };
 
   // Mail configuration
-  if (process.env.MAILGUN_SMTP_LOGIN && process.env.MAILGUN_SMTP_PASSWORD) {
-    console.log('MAILGUN_SMTP_LOGIN and MAILGUN_SMTP_PASSWORD found, setting up SMTP mail transport');
-    config.mail = {
-      transport: 'SMTP',
-      options: {
-        service: 'Mailgun',
-        auth: {
-          user: process.env.MAILGUN_SMTP_LOGIN,
-          pass: process.env.MAILGUN_SMTP_PASSWORD
-        }
+  if (process.env.BREVO_SMTP_LOGIN && process.env.BREVO_SMTP_PASSWORD) {
+  console.log('BREVO_SMTP_LOGIN and BREVO_SMTP_PASSWORD found, setting up SMTP mail transport');
+  config.mail = {
+    transport: 'SMTP',
+    options: {
+      host: 'smtp-relay.brevo.com',
+      port: 587,
+      auth: {
+        user: process.env.BREVO_SMTP_LOGIN,
+        pass: process.env.BREVO_SMTP_PASSWORD
       }
-    };
-  } else {
-    console.log('MAILGUN_SMTP_LOGIN or MAILGUN_SMTP_PASSWORD not found, setting mail transport to Direct');
-    config.mail = {
-      transport: 'Direct'
-    };
-  }
+    }
+  };
+} else {
+  console.log('BREVO_SMTP_LOGIN or BREVO_SMTP_PASSWORD not found, setting mail transport to Direct');
+  config.mail = {
+    transport: 'Direct'
+  };
+}
 
-  // Cloudinary configuration
-  if (process.env.CLOUDINARY_URL) {
-    console.log('CLOUDINARY_URL found, setting storage to cloudinary');
-    config.storage = {
-      active: 'cloudinary',
-      cloudinary: {
-        useDatedFolder: false,
-        upload: {
-          use_filename: true,
-          unique_filename: false,
-          overwrite: false,
-          folder: process.env.CLOUDINARY_FOLDER || 'ghost-blog-images',
-          tags: ['blog']
-        },
-        fetch: {
-          quality: 'auto',
-          secure: true,
-          cdn_subdomain: true
-        }
-      }
-    };
-  } else {
-    console.log('CLOUDINARY_URL not found, setting storage to local');
-    // config.storage = {
-    //   active: 'LocalFileStorage'
-    // };
-  }
 
   // Write the config to the file
   fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
